45b94399eec2bf12ed16d269e885e3583e75a59b
jvn13 <noordjellevan@gmail.com>
2017-11-14 17:19:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jelle/jellevannoord_pset3/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.jelle.jellevannoord_pset3;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_menu:
+                   // mTextMessage.setText(R.string.title_menu);
+                    return true;
+                case R.id.navigation_order:
+                    //mTextMessage.setText(R.string.title_order);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://resto.mprog.nl/categories";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject test = new JSONObject(response);
+                            JSONArray jArray = test.getJSONArray("categories");
+
+                            String[] items = new String[jArray.length()];
+                            if (jArray != null) {
+                                for (int i=0;i<jArray.length();i++){
+                                    items[i] = jArray.getString(i);
+                                }
+                            }
+                            CustomArrayAdapter adapter = new CustomArrayAdapter(MainActivity.this, items);
+                            ListView categoriesList = findViewById(R.id.categoriesList);
+                            categoriesList.setAdapter(adapter);
+                            mTextMessage.setText(Arrays.toString(items));
+                        } catch(JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+
+    }
+}
